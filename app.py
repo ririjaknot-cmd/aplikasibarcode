b4bfcf6ca38db74d52cf9cf3445b703a8617b765
ririjaknot-cmd <riri.jaknot@gmail.com>
2026-06-05 08:02:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ df_database = muat_database()
 # TAMPILAN FORMULIR INPUT VERTIKAL (MENURUN)
 # =========================================================================
 st.subheader("📝 Formulir Input ID")
-st.caption("Tips: Masukkan ID, tekan Tab/Enter untuk pindah ke Jumlah Box. Validasi dilakukan dengan menekan tombol di bawah.")
+st.caption("Tips: Masukkan ID, tekan Tab untuk pindah ke Jumlah Box. Validasi dilakukan dengan menekan tombol di bawah.")
 
 # Form dibuat vertikal menurun ke bawah dengan parameter pengunci enter
 with st.form(key="form_vertikal_shipment", enter_to_submit=False):
