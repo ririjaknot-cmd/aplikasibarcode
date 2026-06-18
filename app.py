3f0116bb254097af26f9d5f814b20ee3c78bce01
Kronus <riri.jaknot@gmail.com>
2026-06-18 23:16:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -194,7 +194,7 @@ if proses_button:
                         html_konten += f"""
                         <div class="kotak-label">
                             <img class="barcode-qr" src="data:image/png;base64,{img_base64}" />
-                            <div class="info-teks">
+                            <div cla ss="info-teks">
                                 <span class="tujuan-bold">{tujuan_terdeteksi}</span><br/>
                                 {id_inputan}<br/>
                                 {b}/{jumlah_box}
